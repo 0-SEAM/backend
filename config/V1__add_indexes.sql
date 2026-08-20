41a0fdd8c63d49bb0e6c41e834308d702499e6d8
vientorepublic <markgod0419@gmail.com>
2026-08-21 02:06:52 +0900

    Fix index naming conventions in SQL migration script for consistency

--- config/V1__add_indexes.sql
+++ config/V1__add_indexes.sql
@@ -7,19 +7,19 @@
 --    - email 유니크 인덱스: 로그인/회원가입 시 이메일 조회 최적화
 --    - refreshToken 인덱스: 로그아웃 시 리프레시 토큰 조회 최적화
 CREATE UNIQUE INDEX idx_user_accounts_email ON user_accounts (email);
-CREATE INDEX idx_user_accounts_refresh_token ON user_accounts (refreshToken);
+CREATE INDEX idx_user_accounts_refresh_token ON user_accounts (refresh_token);
 
 -- 2. field_experiences 테이블
 --    - 복합 인덱스: 지점별 승인된 경험 조회 최적화
 --    - 단일 인덱스: 심사 상태별 조회 최적화
---    (branchId, authorId, visitDate) 유니크 제약조건은 이미 존재
-CREATE INDEX idx_field_exp_branch_moderation ON field_experiences (branchId, moderationStatus);
-CREATE INDEX idx_field_exp_moderation_status ON field_experiences (moderationStatus);
+--    (branch_id, author_id, visit_date) 유니크 제약조건은 이미 존재
+CREATE INDEX idx_field_exp_branch_moderation ON field_experiences (branch_id, moderation_status);
+CREATE INDEX idx_field_exp_moderation_status ON field_experiences (moderation_status);
 
 -- 3. guide_contents 테이블
---    - contentType 인덱스: 콘텐츠 유형별 조회 최적화
-CREATE INDEX idx_guide_content_type ON guide_contents (contentType);
+--    - content_type 인덱스: 콘텐츠 유형별 조회 최적화
+CREATE INDEX idx_guide_content_type ON guide_contents (content_type);
 
 -- 4. timeline_tasks 테이블
 --    - 복합 인덱스: 사용자별 우선순위 정렬 조회 최적화
-CREATE INDEX idx_timeline_user_priority ON timeline_tasks (userId, priority);
+CREATE INDEX idx_timeline_user_priority ON timeline_tasks (user_id, priority);
